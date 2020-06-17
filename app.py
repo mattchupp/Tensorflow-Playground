dbdd8a3890d0e7d0b2c98d8fa07775ca3f39d231
mattchupp <mattlchupp@gmail.com>
2020-06-17 11:38:07 -0500

    moved sarcasm processing to new file

--- app.py
+++ app.py
@@ -2,20 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import json
-
-# open sarcasm.json file
-with open("sarcasm.json", 'r') as f: 
-  datastore = json.load(f)
-
-# map through file and store items in each array
-sentences = []
-labels = []
-urls = []
-for item in datastore: 
-  sentences.append(item['headline'])
-  labels.append(item['is_sarcastic'])
-  urls.append(item['article_link'])
 
 
 # Sentence data
