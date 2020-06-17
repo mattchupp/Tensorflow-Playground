ab00a5fa73532558bf534c837f3002dd2efa0907
mattchupp <mattlchupp@gmail.com>
2020-06-17 10:28:55 -0500

    sequences

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ sentences = [
   'I love my dog',
   'I love my cat',
   'You love my dog!',
-  'I really love my dog'
+  'Do you think my dog is amazing?'
 ]
 
 tokenizer = Tokenizer(num_words = 100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
+
 print(word_index)
+print(sequences)
 
