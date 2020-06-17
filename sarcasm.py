37c5f59ed157692173554af457af1f62da2b28a5
mattchupp <mattlchupp@gmail.com>
2020-06-17 12:00:24 -0500

    import json data

--- sarcasm.py
+++ sarcasm.py
@@ -5,7 +5,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import json
 
 # open sarcasm.json file
-with open("sarcasm.json", 'r') as f: 
+with open("./sarcasm.json", 'r') as f: 
   datastore = json.load(f)
 
 # map through file and store items in each array
@@ -16,4 +16,16 @@ for item in datastore:
   sentences.append(item['headline'])
   labels.append(item['is_sarcastic'])
   urls.append(item['article_link'])
+
+
+tokenizer = Tokenizer(oov_token="<OOV>")
+
+# create token for each word in corpus
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post')
+print(padded[0])
+print(padded.shape)
 
