15ec5ac57943079fe9a96f09eb6f477ec14fa32a
mattchupp <mattlchupp@gmail.com>
2020-06-17 10:32:54 -0500

    test data to test sentences

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-
+# Sentence data
 sentences = [
   'I love my dog',
   'I love my cat',
@@ -10,12 +10,24 @@ sentences = [
   'Do you think my dog is amazing?'
 ]
 
+# other test data
+test_data = [
+  'i really love my dog',
+  'my dog loves my manatee'
+]
+
+# tokenize the words
 tokenizer = Tokenizer(num_words = 100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
 
+# test sequence from test_data
+test_seq = tokenizer.texts_to_sequences(test_data)
+print(test_seq)
+
+# print index of words and squence
 print(word_index)
 print(sequences)
 
